f2f12fb9655af41dbeb8b0f76707b6390e830ac7
Adhitaxl16@gmail.com <tkjasikk@gmail.com>
2021-12-22 12:52:55 +0700

    sintaksis dasar perulangan baca buku dengan for.py

--- sintaksis dasar perulangan baca buku dengan while.py
+++ sintaksis dasar perulangan baca buku dengan while.py
@@ -1,24 +1,23 @@
 """
 program perulangan membaca buku menggunakan while
 """
-jumlah_buku = 10
+book_count = 10
 print('ibu berkata, "baca semua bukumu"')
-jumlah_baca  = 0
+read_count = 0
 
-jumlah_buku_yang_sudah_di_baca_dan_di_pahami = 0
-print(f'jumlah buku yang sudah di baca{jumlah_buku_yang_sudah_di_baca_dan_di_pahami}')
+undertstood_count = 0
+print(f'jumlah buku yang sudah di baca dan di pahami{undertstood_count}')
 
-while jumlah_baca < jumlah_buku * 2:
-    jumlah_baca = jumlah_baca + 1
-    if jumlah_buku_yang_sudah_di_baca_dan_di_pahami == 9:
-        print(f"baca buku ke {jumlah_buku_yang_sudah_di_baca_dan_di_pahami + 1} belum paham")
+while read_count < book_count * 2:
+    read_count = read_count + 1
+    if undertstood_count == 9:
+        print(f"baca buku ke {undertstood_count + 1} belum paham")
     else:
-        jumlah_buku_yang_sudah_di_baca_dan_di_pahami = jumlah_buku_yang_sudah_di_baca_dan_di_pahami + 1
-        print(f"buku ke {jumlah_buku_yang_sudah_di_baca_dan_di_pahami} sudah di baca dan di pahami")
+        undertstood_count = undertstood_count + 1
+        print(f"buku ke {undertstood_count} sudah di baca dan di pahami")
 
-print(f'jumlah buku yang sudah di baca dan dipahami{jumlah_buku_yang_sudah_di_baca_dan_di_pahami}')
-if jumlah_buku_yang_sudah_di_baca_dan_di_pahami == jumlah_buku:
+print(f'jumlah buku yang sudah di baca dan dipahami{undertstood_count}')
+if undertstood_count == book_count:
     print('"Bu, semua buku sudah di baca dan di pahami"')
 else:
-    print(f'Bu, tidak semua buku bisa di pahami. '
-         f'budi hanya bisa memahami{jumlah_buku_yang_sudah_di_baca_dan_di_pahami}')
+    print(f'Bu, tidak semua buku bisa di pahami budi hanya bisa memahami{undertstood_count} buku')
